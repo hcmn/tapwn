262d5d7c4d1bffa5f9f2fc6fb2306060a46beb3f
hc <hc@mobilenerves.com>
2013-08-23 15:01:46 +0000

    db was reset, new demo data, headline model field additions, headline detail template image link fix with prefix http

--- news/models.py
+++ news/models.py
@@ -6,8 +6,21 @@ class Headline(models.Model):
     publication_date = models.DateTimeField(auto_now_add=True)
     subject = models.CharField(max_length=200)
     description = models.TextField()
-    image_link = models.CharField(max_length=200)
+    hotness_label = models.CharField(max_length=200, blank=True)
+    utility_label = models.CharField(max_length=200, blank=True)
+    image_link = models.CharField(max_length=200, blank=True)
+    youtube_link = models.CharField(max_length=200, blank=True)
+    vimeo_link = models.CharField(max_length=200, blank=True)
+    video_link = models.CharField(max_length=200, blank=True)
+    itunes_link = models.CharField(max_length=200, blank=True)
+    appstore_link = models.CharField(max_length=200, blank=True)
+    play_link = models.CharField(max_length=200, blank=True)
+    windows_link = models.CharField(max_length=200, blank=True)
+    steam_link = models.CharField(max_length=200, blank=True)
+    indie_link = models.CharField(max_length=200, blank=True)
+    company_link = models.CharField(max_length=200, blank=True)
     source_link = models.CharField(max_length=200)
+    source2_link = models.CharField(max_length=200, blank=True)
 
     def __unicode__(self):
         return self.subject
